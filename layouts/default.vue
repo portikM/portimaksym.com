f860880483910b3ebb19fea658ed6873ee4ee9b6
Maksym P <max@rumieadmins-MacBook-Pro.local>
2020-04-12 18:17:23 -0400

    Add fonts and assets

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,10 +4,85 @@
   </div>
 </template>
 
-<style>
+<style lang="scss">
+@font-face {
+  font-family: 'Circular Std';
+  src: url('~assets/fonts/CircularStd-Book.eot'),
+    url('~assets/fonts/CircularStd-Book.ttf') format('ttf'),
+    url('~assets/fonts/CircularStd-Book.woff') format('woff'),
+    url('~assets/fonts/CircularStd-Book.woff2') format('woff2');
+  font-weight: normal;
+}
+
+@font-face {
+  font-family: 'Circular Std';
+  src: url('~assets/fonts/CircularStd-BookItalic.eot'),
+    url('~assets/fonts/CircularStd-BookItalic.ttf') format('ttf'),
+    url('~assets/fonts/CircularStd-BookItalic.woff') format('woff'),
+    url('~assets/fonts/CircularStd-BookItalic.woff2') format('woff2');
+  font-weight: normal;
+  font-style: italic;
+}
+
+@font-face {
+  font-family: 'Circular Std';
+  src: url('~assets/fonts/CircularStd-Medium.eot'),
+    url('~assets/fonts/CircularStd-Medium.ttf') format('ttf'),
+    url('~assets/fonts/CircularStd-Medium.woff') format('woff'),
+    url('~assets/fonts/CircularStd-Medium.woff2') format('woff2');
+  font-weight: 600;
+}
+
+@font-face {
+  font-family: 'Circular Std';
+  src: url('~assets/fonts/CircularStd-MediumItalic.eot'),
+    url('~assets/fonts/CircularStd-MediumItalic.ttf') format('ttf'),
+    url('~assets/fonts/CircularStd-MediumItalic.woff') format('woff');
+  font-weight: 600;
+  font-style: italic;
+}
+
+@font-face {
+  font-family: 'Circular Std';
+  src: url('~assets/fonts/CircularStd-Bold.eot'),
+    url('~assets/fonts/CircularStd-Bold.ttf') format('ttf'),
+    url('~assets/fonts/CircularStd-Bold.woff') format('woff'),
+    url('~assets/fonts/CircularStd-Bold.woff2') format('woff2');
+  font-weight: bold;
+}
+
+@font-face {
+  font-family: 'Circular Std';
+  src: url('~assets/fonts/CircularStd-BoldItalic.eot'),
+    url('~assets/fonts/CircularStd-BoldItalic.ttf') format('ttf'),
+    url('~assets/fonts/CircularStd-BoldItalic.woff') format('woff'),
+    url('~assets/fonts/CircularStd-BoldItalic.woff2') format('woff2');
+  font-weight: bold;
+  font-style: italic;
+}
+
+@font-face {
+  font-family: 'Circular Std';
+  src: url('~assets/fonts/CircularStd-Black.eot'),
+    url('~assets/fonts/CircularStd-Black.ttf') format('ttf'),
+    url('~assets/fonts/CircularStd-Black.woff') format('woff'),
+    url('~assets/fonts/CircularStd-Black.woff2') format('woff2');
+  font-weight: 900;
+}
+
+@font-face {
+  font-family: 'Circular Std';
+  src: url('~assets/fonts/CircularStd-BlackItalic.eot'),
+    url('~assets/fonts/CircularStd-BlackItalic.ttf') format('ttf'),
+    url('~assets/fonts/CircularStd-BlackItalic.woff') format('woff'),
+    url('~assets/fonts/CircularStd-BlackItalic.woff2') format('woff2');
+  font-weight: 900;
+  font-style: italic;
+}
+
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: 'Circular Std', 'Source Sans Pro', -apple-system,
+    BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -24,32 +99,35 @@ html {
   margin: 0;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+// extending tailwind
+
+.text-porti-black {
+  color: #0c0c0c;
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+.bg-porti-black {
+  background-color: #0c0c0c;
 }
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
+.rounded-2xl {
+  border-radius: 2rem;
 }
 
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+.z-1000 {
+  z-index: 1000;
+}
+
+// typorgaphy
+
+h1 {
+  @apply text-4xl text-porti-black font-black;
+}
+
+h2 {
+  @apply text-2xl text-porti-black font-black;
+}
+
+h3 {
+  @apply text-xl text-porti-black font-bold;
 }
 </style>
