6c18683a5a7074639392d66afbf4d164a4c78349
portikM <maksym.portianoi@konghq.com>
2024-05-25 12:57:49 -0400

    fix: articles nav styling

--- components/ArticlesNav.vue
+++ components/ArticlesNav.vue
@@ -6,12 +6,13 @@
     <nav>
       <ul>
         <li v-for="article in articles" :key="article.slug">
-          <router-link :to="{ path: article.slug }" class="min-h-32 p-3 md:pl-12 flex items-center gap-5 mb-10 rounded-lg transition-colors group hover:bg-violet-500 outline-none focus-visible:ring-2 focus-visible:ring-pink-500 relative">
+          <router-link :to="{ path: article.slug }" class="min-h-32 p-3 lg:pl-12 flex items-center gap-5 mb-10 rounded-lg transition-colors group hover:bg-violet-500 outline-none focus-visible:ring-2 focus-visible:ring-pink-500 relative">
             <div class="flex flex-col w-1/2">
-              <span class="text-xl sm:text-2xl font-bold mb-1 leading-6 group-hover:text-white">{{ article.title }}</span>
+              <span class="text-xl lg:text-2xl font-bold mb-1 leading-6 group-hover:text-white">{{ article.title }}</span>
+              <span class="mb-3 text-sm group-hover:text-gray-100">{{ article.description }}</span>
               <span class="text-sm text-gray-400 group-hover:text-gray-100">Published on {{ article.published }}</span>
             </div>
-            <div class="w-40 h-28 rounded-lg absolute right-0 scale-125 md:right-[20%] -rotate-3 shadow-lg">
+            <div class="w-40 h-28 rounded-lg absolute right-8 scale-125 lg:right-[20%] -rotate-3 shadow-lg">
               <img :src="`/images/${article.image}`" alt="" class="object-cover object-center w-full h-full rounded-lg" />
             </div>
           </router-link>
@@ -49,6 +50,7 @@ const { data } = await useAsyncData('articles', () =>
 
 const articles = data.value.map(entry => ({
   title: entry.title,
+  description: entry.description,
   published: entry.published,
   slug: entry._path.replace('/articles/', ''),
   image: entry.image,
---
+++ content/articles/building-design-system-at-kong.md
@@ -0,0 +1,7 @@
+---
+title: Things I learned while building design system at Kong
+description: I refactored our entire open-source UI component library. Here are a few lessons I learned.
+published: May 24, 2024
+image: kong_design_system_banner.png
+index: 0
+---
